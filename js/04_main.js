bcc4745ae38dcdbb42b53430afa1da4e45e3d710
Mogu571 <86176073+Mogu571@users.noreply.github.com>
2025-10-06 11:42:09 +0800

    Update 04_main.js

--- js/04_main.js
+++ js/04_main.js
@@ -9,10 +9,13 @@ document.addEventListener("DOMContentLoaded", () => {
             console.log("被试姓名：", GLOBAL_DATA.subjectName);
             console.log("实验数据预览：", GLOBAL_DATA.experimentLog.slice(0, 5)); // 打印前5行数据
         },
-        // 可选：开启数据列配置
-        show_progress_bar: false, // 是否显示进度条
-        message_progress_bar: '完成进度', // 进度条文字
+        // 可选：显示进度条
+        show_progress_bar: false,
+        message_progress_bar: '完成进度'
     });
+
+    // 随机打乱图片呈现顺序（复现MATLAB的randperm(100)）
+    IMAGE_LIST = jsPsych.randomization.shuffle(IMAGE_LIST);
 
     // 运行实验时间线
     jsPsych.run(EXPERIMENT_TIMELINE);
